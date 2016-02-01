777c4c40a201a0194ce7827d2bec543839cc96e1
Manoel Menezes <manoel.menezes.jr@gmail.com>
2016-02-01 02:59:36 -0800

    version with index of terms

--- src/main/java/com/sortable/domain/Result.java
+++ src/main/java/com/sortable/domain/Result.java
@@ -1,19 +1,15 @@
 package com.sortable.domain;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Result {
 
 	private String product_name;
-	private List<Listing> listings;
 	private Set<Listing> listingsSet;
 
 	public Result(String productName) {
 		this.product_name = productName;
-		this.listings = new ArrayList<Listing>();
 		this.listingsSet = new HashSet<Listing>();
 	}
 
@@ -22,12 +18,15 @@ public class Result {
 	}
 
 	public void add(Listing listing) {
-		listings.add(listing);
-		listingsSet.addAll(listings);
+		listingsSet.add(listing);
 	}
-	
+
 	public boolean isEmpty() {
 		return listingsSet.isEmpty();
+	}
+
+	public void remove(Listing listing) {
+		listingsSet.remove(listing);
 	}
 
 }
--- src/main/java/com/sortable/service/MatchingService.java
+++ src/main/java/com/sortable/service/MatchingService.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.Set;
 
 import com.sortable.domain.Listing;
-import com.sortable.domain.MatchingRelevance;
 import com.sortable.domain.Product;
 import com.sortable.domain.Result;
 
@@ -25,26 +24,6 @@ public class MatchingService {
 	}
 
 	public Result[] match() {
-		Product[] products = productService.getProducts();
-		Listing[] listings = listingService.getListings();
-
-		Map<Product, Result> results = new HashMap<Product, Result>();
-
-		for (Listing listing : listings) {
-			Product product = searchTopOneProduct(listing, products);
-
-			if (product != null) {
-				if (!results.containsKey(product)) {
-					results.put(product, new Result(product.getName()));
-				}
-				results.get(product).add(listing);
-			}
-		}
-
-		return results.values().toArray(new Result[] {});
-	}
-
-	public Result[] matchUsingIndex() {
 		Product[] products = productService.getProducts();
 		Listing[] listings = listingService.getListings();
 
@@ -76,17 +55,40 @@ public class MatchingService {
 			Map<String, Set<Listing>> indexOfTermsToListings) {
 
 		List<Result> results = new ArrayList<Result>();
-		
-		Set<Listing> listingsMatched = new HashSet<Listing>();
+
+		Map<Listing, Product> listingsMatched = new HashMap<Listing, Product>();
+		Map<Product, Result> productsMatched = new HashMap<Product, Result>();
 
 		for (Product product : products) {
 			Result result = new Result(product.getName());
+			productsMatched.put(product, result);
 
 			for (Listing listing : possibleMatchingsWithModel(product,
 					indexOfTermsToListings)) {
-				if (!listingsMatched.contains(listing) && manufacturerMatches(product, listing)) {
-					listingsMatched.add(listing);
-					result.add(listing);
+				if (manufacturerMatches(product, listing)) {
+
+					if (listingsMatched.containsKey(listing)) {
+
+						Product productMatched = listingsMatched.get(listing);
+
+						if (productMatched.getModelLower().length() < product
+								.getModelLower().length()) {
+							listingsMatched.put(listing, product);
+							productsMatched.get(productMatched).remove(listing);
+
+							if (productsMatched.get(productMatched).isEmpty()) {
+								results.remove(productsMatched
+										.get(productMatched));
+							}
+
+							result.add(listing);
+						}
+
+					} else {
+						listingsMatched.put(listing, product);
+						result.add(listing);
+					}
+
 				}
 			}
 
@@ -154,150 +156,4 @@ public class MatchingService {
 		return result;
 	}
 
-	private Product searchTopOneProduct(Listing listing, Product[] products) {
-		MatchingRelevance bestMatchingRelevance = MatchingRelevance.NO_RELEVANCE;
-		Product topOneProduct = null;
-
-		for (Product product : products) {
-
-			String listingManufacturer = listing.getManufacturer()
-					.toLowerCase();
-			String productManufacturer = product.getManufacturer()
-					.toLowerCase();
-
-			if (listingManufacturer.contains(productManufacturer)
-					|| productManufacturer.contains(listingManufacturer)) {
-
-				if (listing.getTitle().toLowerCase()
-						.contains(product.getName().toLowerCase())) {
-					return product;
-				}
-
-				MatchingRelevance mr = macthingRelevance(listing, product);
-
-				if (mr.isBetterThan(bestMatchingRelevance)) {
-					bestMatchingRelevance = mr;
-					topOneProduct = product;
-				}
-			}
-		}
-
-		if (bestMatchingRelevance.hasHighPrecision()) {
-			return topOneProduct;
-		}
-
-		return null;
-	}
-
-	private MatchingRelevance macthingRelevance(Listing listing, Product product) {
-		boolean containsModel = contains(listing.getTitle(), product.getModel());
-
-		if (product.hasFamily()) {
-			boolean containsFamily = contains(listing.getTitle(),
-					product.getFamily());
-
-			if (containsFamily && containsModel) {
-				return MatchingRelevance.MODEL_AND_FAMILY;
-			}
-
-			if (containsFamily && !containsModel) {
-				return MatchingRelevance.FAMILY_AND_NOT_MODEL;
-			}
-
-			if (!containsFamily && containsModel) {
-				return MatchingRelevance.MODEL_AND_NOT_FAMILY;
-			}
-		} else if (containsModel) {
-			return MatchingRelevance.MODEL;
-		}
-
-		return MatchingRelevance.NO_RELEVANCE;
-	}
-
-	private boolean contains(String source, String pattern) {
-		source = source.toLowerCase();
-		pattern = pattern.toLowerCase();
-
-		if (containsHelper(source, pattern)) {
-			return true;
-		}
-
-		String patternWithoutSpaces = pattern.replace(" ", "");
-
-		if (containsHelper(source, patternWithoutSpaces)) {
-			return true;
-		}
-
-		String patternWithoutDashes = pattern.replace("-", "");
-
-		if (containsHelper(source, patternWithoutDashes)) {
-			return true;
-		}
-
-		String patternWithoutUnderline = pattern.replace("_", "");
-
-		if (containsHelper(source, patternWithoutUnderline)) {
-			return true;
-		}
-
-		String patternWithoutSpacesAndDashes = pattern.replace(" ", "")
-				.replace("-", "");
-
-		if (containsHelper(source, patternWithoutSpacesAndDashes)) {
-			return true;
-		}
-
-		String patternWithoutSpacesAndUnderlines = pattern.replace(" ", "")
-				.replace("_", "");
-
-		if (containsHelper(source, patternWithoutSpacesAndUnderlines)) {
-			return true;
-		}
-
-		String patternWithoutDashesAndUnderlines = pattern.replace("-", "")
-				.replace("_", "");
-
-		if (containsHelper(source, patternWithoutDashesAndUnderlines)) {
-			return true;
-		}
-
-		String patternWithoutSpacesDashesAndUnderlines = pattern
-				.replace(" ", "").replace("-", "").replace("_", "");
-
-		if (containsHelper(source, patternWithoutSpacesDashesAndUnderlines)) {
-			return true;
-		}
-
-		String patternWithDashes = pattern.replace(" ", "-").replace("_", "-");
-
-		if (containsHelper(source, patternWithDashes)) {
-			return true;
-		}
-
-		String patternWithUnderline = pattern.replace(" ", "_").replace("-",
-				"_");
-
-		if (containsHelper(source, patternWithUnderline)) {
-			return true;
-		}
-
-		String patternWithSpaces = pattern.replace("-", " ").replace("_", " ");
-
-		if (containsHelper(source, patternWithSpaces)) {
-			return true;
-		}
-
-		return false;
-	}
-
-	private boolean containsHelper(String source, String pattern) {
-		if (source.equals(pattern)) {
-			return true;
-		}
-
-		return source.startsWith(pattern + " ")
-				|| source.endsWith(" " + pattern)
-				|| source.contains(" " + pattern + " ");
-
-	}
 }
